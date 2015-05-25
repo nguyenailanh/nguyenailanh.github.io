dd711635ee92d2ff9b786adc1d6285199f67fc87
ctran <cuong.tran@sutrixmedia.com>
2015-05-25 14:17:05 +0700

    add author

--- youtube/auth.js
+++ youtube/auth.js
@@ -2,7 +2,7 @@
 // at {{ https://cloud.google.com/console }}.
 // If you run this code from a server other than http://localhost,
 // you need to register your own client ID.
-var OAUTH2_CLIENT_ID = '__YOUR_CLIENT_ID__';
+var OAUTH2_CLIENT_ID = '850710170446-t8q5m4gmqr3haf6kc104c2ca91tm8og8.apps.googleusercontent.com';
 var OAUTH2_SCOPES = [
   'https://www.googleapis.com/auth/youtube'
 ];
